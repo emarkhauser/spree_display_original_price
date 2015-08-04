7c6fe6167f7652e79cdd2cf907b5bf049e16c960
Erik Markhauser <emarkhauser@yahoo.com>
2015-08-04 19:49:19 -0400

    Format original price data form in product edit to match other data elements

--- app/overrides/add_original_price_to_product_edit.rb
+++ app/overrides/add_original_price_to_product_edit.rb
@@ -1,11 +1,12 @@
 Deface::Override.new(:virtual_path => 'spree/admin/products/_form',
   :name => 'add_original_price_to_product_edit',
-  :insert_after => "erb[loud]:contains('text_field :price')",
+  :insert_after => "div[data-hook=admin_product_form_price]",
   :text => "
-    <%= f.field_container :original_price do %>
-      <%= f.label :original_price, raw(Spree.t(:original_price) + content_tag(:span, ' *')) %>
-      <%= f.text_field :original_price, :value =>
-        number_to_currency(@product.original_price, :unit => '') %>
-      <%= f.error_message_on :original_price %>
-    <% end %>
+    <div data-hook='admin_product_form_original_price'>
+      <%= f.field_container :original_price, class: ['form-group'] do %>
+        <%= f.label :original_price, raw(Spree.t(:original_price)) %>
+        <%= f.text_field :original_price, :value => number_to_currency(@product.original_price, :unit => ''), class: 'form-control' %>
+        <%= f.error_message_on :original_price %>
+      <% end %>
+    </div>
   ")
